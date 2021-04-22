282aa27b026b5b045a6a6ccedd096830aff4d277
BinkitaTiwari <135starmoon@gmail.com>
2021-04-23 01:02:02 +0530

    created the register page and did the authencation using firebase

---
+++ src/components/Firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var config = {
+    apiKey: "AIzaSyA-Aw3dfctNaXXXWDYzQ9L9Z4qawGUV7cs",
+    authDomain: "slack-app-38bff.firebaseapp.com",
+    projectId: "slack-app-38bff",
+    storageBucket: "slack-app-38bff.appspot.com",
+    messagingSenderId: "1000536932417",
+    appId: "1:1000536932417:web:bf149fc68bf05f9b2bc6c0",
+    measurementId: "G-WD3KYC5QH3"
+  };
+
+  firebase.initializeApp(config);
+
+  export default firebase;

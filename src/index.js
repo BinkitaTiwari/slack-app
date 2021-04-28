86a714ff28eb8acfb09c5b46f5d33e9b525d6b7c
BinkitaTiwari <135starmoon@gmail.com>
2021-04-28 13:07:13 +0530

    added redux, signout functionality

--- src/index.js
+++ src/index.js
@@ -6,22 +6,36 @@ import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
-
+import Spinner from "./Spinner";
 import "semantic-ui-css/semantic.min.css";
 
 import { BrowserRouter as Router, Switch, Route, withRouter} from "react-router-dom";
+
+
+import { createStore } from "redux";
+import { Provider,connect} from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import {setUser,clearUser} from "./actions/index";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends Component{
 componentDidMount(){
   firebase.auth().onAuthStateChanged(user=>{
     if(user){
+      this.props.setUser(user)
       this.props.history.push('/')
+    }
+    else{
+      this.props.history.push('/login');
+      this.props.clearUser();
     }
   })
 }
 
 render(){
-  return(
+  return this.props.isLoading? <Spinner/> :(
   
     <Switch>
       <Route exact path="/" component={App} />
@@ -35,11 +49,20 @@ render(){
 
 
 }
-  const RootWithAuth= withRouter(Root);
+
+const mapStateToProps= state=>{
+  return{
+    isLoading: state.user.isLoading
+  }
+}
+  const RootWithAuth= withRouter(connect(mapStateToProps, {setUser,clearUser})(Root));
 
 ReactDOM.render(
-<Router>
-<RootWithAuth />
-</Router>
+  <Provider store={store}>
+  <Router>
+  <RootWithAuth />
+  </Router>
+  </Provider>
+
 , document.getElementById("root"));
 registerServiceWorker();

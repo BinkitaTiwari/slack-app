aa6b99bbc6c65d7f254d5a0ccc2bd8c482bc533f
BinkitaTiwari <135starmoon@gmail.com>
2021-05-05 12:04:10 +0530

    created the starred component and added the functionality of starring and unstarring the channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 import UserPanel from "./UserPanel";
 import Channels from "./Channels";
+import Starred from "./Starred";
 import DirectMessages from "./DirectMessages";
 
 class SidePanel extends React.Component {
@@ -16,7 +17,9 @@ class SidePanel extends React.Component {
         vertical
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
+        
         <UserPanel currentUser={currentUser}/>
+        <Starred currentUser={currentUser}/>
         <Channels currentUser={currentUser}/>
         <DirectMessages currentUser={currentUser}/>
       </Menu>

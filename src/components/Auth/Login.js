6ac2b8f4125e05158837f29a0035ed6fb7919621
Binkita Tiwari <135st@BINTIWAR-MPUBX.cisco.com>
2021-07-15 13:11:29 +0530

    testing

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -13,7 +13,7 @@ import { Link } from "react-router-dom";
 
 class Login extends React.Component {
   state = {
-    email: "",
+    email: "" ,
     password: "",
     errors: [],
     loading: false
